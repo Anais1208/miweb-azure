537332df3f93ea2e25b49ac8acc001f1aab7ecf5
Anais1208 <124419476+Anais1208@users.noreply.github.com>
2025-10-06 16:27:05 -0300

    Update desplegar.sh

--- desplegar.sh
+++ desplegar.sh
@@ -5,8 +5,6 @@ VM_NAME="MiVM"
 RESOURCE_GROUP="MiGrupo"
 LOCATION="East US"
 
-# Crear VM (opcional si ya está creada)
-# az vm create --resource-group $RESOURCE_GROUP --name $VM_NAME --image UbuntuLTS --admin-username miusuario --generate-ssh-keys --location "$LOCATION"
 
 # Abrir puerto 80 en NSG
 az network nsg rule create \
@@ -21,21 +19,19 @@ az network nsg rule create \
   --destination-port-ranges 80 \
   --access Allow
 
-# Instalar Nginx
 az vm run-command invoke \
   --command-id RunShellScript \
   --name $VM_NAME \
   --resource-group $RESOURCE_GROUP \
   --scripts "sudo apt update && sudo apt install -y nginx"
 
-# Subir index.html a /var/www/html
 az vm run-command invoke \
   --command-id RunShellScript \
   --name $VM_NAME \
   --resource-group $RESOURCE_GROUP \
   --scripts "echo '<html><body><h1>Mi MV Azure</h1><p>Desplegado vía Azure CLI</p></body></html>' | sudo tee /var/www/html/index.html"
 
-# Iniciar Nginx
+
 az vm run-command invoke \
   --command-id RunShellScript \
   --name $VM_NAME \
